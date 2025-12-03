13484e117a2ea5193ec68e0c4a4c90b92f5b6873
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2025-12-03 17:00:34 +0530

    Add logical and arithmetic operations examples
    Added logicalOps.js with examples of logical operators (&&, ||, !) and operations.js with examples of arithmetic, comparison, assignment operations, and operator precedence in JavaScript.

---
+++ JS Foundation/logicalsOps.js
@@ -0,0 +1,17 @@
+//Logical Operations
+
+// && -> and
+// || -> or
+// ! -> reverse
+
+let islogin = true;
+let ispaid = false;
+
+console.log(islogin && ispaid); //false
+console.log(islogin || ispaid); //true
+console.log(!islogin); //false
+
+let idEmailUser = true;
+let idEmailAdmin = false;
+
+console.log(idEmailUser && idEmailAdmin); //false
---
+++ JS Foundation/operations.js
@@ -0,0 +1,44 @@
+//Arithmatic Operations
+
+let add = 2 + 3;
+
+let addition = 4 + 5;
+let subtract = 9 - 3;
+let mult = 3 * 5;
+let division  = 8 / 2;
+let remainder = 9 % 2;
+let expo = 2 ** 3;
+
+let myscore = 90;
+myscore++;
+
+let credits = 100;
+credits--;  
+
+//Comparison Operations
+
+let num1 = 7;
+let num2 = 3;
+let num3 = 3;
+console.log(num1 > num2); //true
+console.log(num2 < num1); //true
+console.log(num2 >= num3); //true
+console.log(num2 <= num3); //true
+console.log(num2 == num3); //true
+console.log(num2 != num3); //false
+
+
+// Assignment Operations
+
+let score = 100;
+score += 20;
+score -= 10;
+score *= 2;
+score /= 2;
+score %= 3;
+score **= 2;
+console.log(score);
+
+// Operator Precedence
+
+let result = (5*(3 + 5) - 2) / 2;
