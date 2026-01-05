a41c731848345657315c9d5e983110924b5d58a0
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-01-06 01:03:59 +0530

    Add OOP concepts: encapsulation, abstraction, polymorphism
    Added examples for encapsulation (BankAccount), abstraction (CoffeeMachine), polymorphism (Bird and penguin), and getter/setter usage (Employee) to illustrate key OOP principles in JavaScript.

--- 6_JS OOPs/OOPS_Learnings.js
+++ 6_JS OOPs/OOPS_Learnings.js
@@ -65,3 +65,87 @@ console.log(myCar.drive());
 let Vehicle1 = new Vehicle('Ford', 'Mustang');
 console.log(Vehicle1.brand());
 
+// Encapsulation
+
+class BankAccount {
+    #balance = 0;
+
+    deposit(amount) {
+        this.#balance += amount;
+        return this.#balance;   
+    }
+    getBalance() {
+        return `$ ${this.#balance}`;
+    }
+}
+
+let myAccount = new BankAccount();
+console.log(myAccount.getBalance());
+
+
+// Abstraction
+
+class CoffeeMachine {
+    start(){
+        return 'Coffee Machine Started';
+    }
+    #brewCoffee() {
+        return 'Brewing Coffee';
+    }
+}
+
+let myCoffeeMachine = new CoffeeMachine();
+console.log(myCoffeeMachine.start());
+// console.log(myCoffeeMachine.#brewCoffee()); // Error: Private method
+
+//Polymorphism
+
+class Bird {
+    fly() {
+        return 'Flying';
+    }
+}
+
+class penguin extends Bird {
+    fly() {
+        return 'Penguins cannot fly';
+    }
+}
+
+let bird = new Bird();
+let penguin = new penguin();
+console.log(bird.fly());
+console.log(penguin.fly());
+
+
+
+// Getter and Setter
+
+class Employee {
+    #salary;
+    constructor(name, salary) {
+
+        if ( salary < 0) {
+            throw new Error('Salary cannot be negative');
+        }
+        this._name = name;
+        this.#salary = salary;
+    }
+
+    get salary() {
+        return `You are not allowed to access salary`;
+    }
+
+    set salary(value) {
+       if (value < 0) {
+           console.log('Salary cannot be negative');
+       } else {
+           this._salary = value;
+       }
+    }
+}
+   let emp = new Employee('Ram', 50000);
+   console.log(emp._salary); 
+    emp.salary = -1000; // Output: Salary cannot be negative
+   console.log(emp.salary); // Output: You are not allowed to access salary
+
